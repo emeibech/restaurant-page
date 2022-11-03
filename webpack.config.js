4fa679c5d1e6bb9d6c07a49d439658581fd44907
emeibech <emeibech@gmail.com>
2022-11-03 15:34:26 +0800

    Get assets

--- webpack.config.js
+++ webpack.config.js
@@ -36,12 +36,17 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource'
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Restaurant Page',
+            title: 'Good Eatin\'',
             filename: 'index.html',
             template: 'src/template.html'
         })
