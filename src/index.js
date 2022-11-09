203479dc1928f2750b5d3ad0330f5c04d4d77de9
emeibech <emeibech@gmail.com>
2022-11-09 23:27:51 +0800

    Return default page to home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'normalize.css';
 import './styles/styles.css';
 import { home } from './home/home';
-import { contact } from "./contact/contact";
 
-document.addEventListener('DOMContentLoaded', contact);
+document.addEventListener('DOMContentLoaded', home);
